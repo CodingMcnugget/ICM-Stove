0c20082ac5dc8f624ff2788a16671ca4aeae6075
CodingMcnugget <144944897+CodingMcnugget@users.noreply.github.com>
2023-11-30 11:34:55 -0500

    Update sketch.js

--- week10/sound_YiheandLening_2023_11_29_04_29_00 copy/sketch.js
+++ week10/sound_YiheandLening_2023_11_29_04_29_00 copy/sketch.js
@@ -1,28 +1,20 @@
-
-
-let img;
-
-function preload() {
-  img = loadImage('Pixel 2.png');
-}
-
-let movingLines1 = [];
+let img;// backgournd 
+let movingLines1 = []; 
 let movingLines2 = [];
-// let lastPlayTime = 0
 let oscillators = {
-    sine: {},
+    sine: {}, //timbre
     square: {},
     triangle: {},
     sawtooth: {}
 };
-let blocks = []; // 用于存储所有方块
+let blocks = []; 
 let chords = {
     "C": ["C4", "E4", "G4"],
     "G": ["G4", "B4", "D5"],
     "A": ["A4", "C5", "E5"],
     "F": ["F4", "A4", "C5"],
     "D": ["D4", "F4", "A4"],
-    "C5": ["C4", "E4", "G4", "B4", "D5"],  // C大七和弦加九度
+    "C5": ["C4", "E4", "G4", "B4", "D5"],  // C大七和弦加九度 （Chords in chinese）
     "D5": ["D4", "F4", "A4", "C5", "E5"],  // D小七和弦加九度
     "E5": ["E4", "G4", "B4", "D5", "F5"],  // E小七和弦加减九度
     "F5": ["F4", "A4", "C5", "E5", "G5"],  // F大七和弦加九度
@@ -32,6 +24,10 @@ let chords = {
 };
 
 
+function preload() {
+  img = loadImage('Pixel 2.png');
+}
+
 function setup() {
     img.loadPixels();
     background(220);
@@ -40,7 +36,7 @@ function setup() {
      
         new MovingLine(0, windowHeight, windowWidth / 2, windowHeight / 2),
         new MovingLine(windowWidth / 2, windowHeight, windowWidth, windowHeight / 2)
-    ];
+    ]; // push new movinglines into array
 
     movingLines2 = [
         new MovingLine(0, windowHeight / 2, windowWidth / 2, 0),
@@ -59,15 +55,16 @@ function setup() {
             return acc;
         }, {});
     });
+  
     initializeBlocks();
-
+// function that push all blocks into an array
     
  
 }
 
 
 
-//和弦时间太短可能会出bug
+//set the play duration below 0.5 will break the Browser
 function playChord(chordNotes, type) {
     let now = Tone.now();
     chordNotes.forEach(note => {
@@ -83,16 +80,17 @@ function playChord(chordNotes, type) {
 document.getElementById('startAudio').addEventListener('click', function() {
     Tone.start().then(() => {
         console.log('AudioContext successfully started');
-        // 这里可以进行其他与音频相关的操作
+        // test if people interacted with the browser
     }).catch(e => {
         console.error('Error starting AudioContext', e);
+      // test if people failed interacted with the browser
     });
 });
 
 document.getElementById('startLines').addEventListener('click', function() {
-    movingLines1.forEach(line => line.start());
+    movingLines1.forEach(line => line.start()); 
     movingLines2.forEach(line => line.start());
-});
+}); // button for start moving the line
 
 
 
@@ -102,30 +100,17 @@ function draw() {
     stroke(0);
     strokeWeight(1);
 
-    // 水平线和垂直线
+    // line devide screen
     line(0, windowHeight / 2, windowWidth, windowHeight / 2);
     line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
 
-    // 更新和绘制方块
+    // draw and update the block
     blocks.forEach(block => {
         block.update();
         block.draggableblockDraw();
-
-        // // 对于每条移动线，检查是否与方块重叠
-        // movingLines1.forEach(line => {
-        //     if (line.x3 > block.x && line.x3 < block.x + block.w) {
-        //         block.playChord();
-                
-        //     }
-        // });
-        // movingLines2.forEach(line => {
-        //     if (line.x4 > block.x && line.x4 < block.x + block.w) {
-        //         block.playChord();
-        //     }
-        // });
     });
     
-    // 更新和绘制移动的线
+    // draw and update the line
     movingLines1.forEach(line => {
         line.update1();
         line.draw1();
@@ -137,6 +122,7 @@ function draw() {
     });
 
     updateCursorForBlocks()
+    // function that change cursor
 }
 
 
@@ -155,25 +141,25 @@ class DraggableBlock {
       this.w = w;
       this.h = h;
       this.chord = chord;
-      this.dragging = false; // 是否正在拖拽
-      this.resizingRight = false; // 是否正在调整右边缘大小
-      this.resizingLeft = false; // 是否正在调整左边缘大小
-      this.dragOffsetX = 0; // 拖拽时的水平偏移量
-      this.dragOffsetY = 0; // 拖拽时的垂直偏移量
-      this.resizeMargin = 10; // 调整大小区域的边距
+      this.dragging = false; // check if dargging
+      this.resizingRight = false; // chech if rightside of the block is changing
+      this.resizingLeft = false; // chech if leftside of the block is changing
+      this.dragOffsetX = 0; // how much moved horizontally
+      this.dragOffsetY = 0; // how much moved vertically
+      this.resizeMargin = 10; // resize margin
     }
   
     draggableblockDraw() {
-        // 绘制方块
+     
         noStroke();
         fill(255, 0, 20);
         rect(this.x, this.y, this.w, this.h*2);
 
-        // 显示和弦
+        // text
         fill(255);
         text(this.chord, this.x + this.w / 3, this.y + this.h*4/3 );
 
-        // 检查是否和任何移动线重叠
+        // check if a moving line is hover
         movingLines1.forEach(line => {
             if (line.x3 > this.x && line.x3 < this.x + this.w && line.y1 > this.y && line.y2 <this.y + this.h*2) {
                 this.playChord();
@@ -189,7 +175,7 @@ class DraggableBlock {
     
 
   
-    // 更新方块状态
+    // update
   update() {
     
     let overLeftEdge = mouseX > this.x && mouseX < this.x + this.resizeMargin;
@@ -197,18 +183,18 @@ class DraggableBlock {
     let overVerticalRange = mouseY > this.y && mouseY < this.y + this.h;
     let insideBlock = mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h;
   
-    // 如果正在调整大小
+   
     if (this.resizingRight) {
-      this.w = max(20, mouseX - this.x); // 设置最小宽度并调整宽度
+      this.w = max(20, mouseX - this.x); // limit the minimum size 
     } else if (this.resizingLeft) {
       let newWidth = this.x + this.w - mouseX;
-      if (newWidth > 20) { // 设置最小宽度
+      if (newWidth > 20) { // limit the minimum size 
         this.x = mouseX;
         this.w = newWidth;
       }
     }
   
-    // 如果正在拖拽，则更新方块位置
+    // change location
     if (this.dragging) {
       this.x = mouseX + this.dragOffsetX;
       this.y = mouseY + this.dragOffsetY;
@@ -220,7 +206,7 @@ class DraggableBlock {
       };
   }
   
-    // 处理鼠标按下事件
+    // check if dragging
     pressed() {
       let overVerticalRange = mouseY > this.y && mouseY < this.y + this.h;
       let overLeftEdge = mouseX > this.x && mouseX < this.x + this.resizeMargin;
@@ -236,7 +222,7 @@ class DraggableBlock {
       }
     }
   
-    // 处理鼠标释放事件
+    // check if not dragging
     released() {
       this.dragging = false;
       this.resizingRight = false;
@@ -255,7 +241,7 @@ class DraggableBlock {
     }
 
     getOscillatorType() {
-        // 根据方块所在区域返回相应的波形类型
+        // chack the tone according to the area
         if (this.y < windowHeight / 2) {
             return this.x < windowWidth / 2 ? 'sine' : 'square';
         } else {
@@ -277,20 +263,19 @@ class DraggableBlock {
         this.x3 = x1;
         this.x4 = x2
         this.y = y1;
-        this.moving = false; // 标记是否开始移动
-        this.speed = 2; // 这里设定一个初始速度值，可以根据需要调整
+        this.moving = false; 
+        this.speed = 2; 
     }
 
     start() {
-        this.moving = true; // 启动线的运动
+        this.moving = true;
     }
 
     update1() {
         if (!this.moving) return;
-        // 更新线的位置
-        this.x3 += 4; // 以固定速度移动
+        this.x3 += 4; 
         if (this.x3 > windowWidth) {
-            this.x3 = this.x1; // 重置到初始位置
+            this.x3 = this.x1; // go back to the start point
         }
     }
 
@@ -298,7 +283,7 @@ class DraggableBlock {
         if (!this.moving) return;
         this.x4 += this.speed;
         if (this.x4 > windowWidth) {
-            this.x4 = this.x1; // 重置到初始位置
+            this.x4 = this.x1; 
         }
     }
 
@@ -325,10 +310,9 @@ class DraggableBlock {
 function initializeBlocks() {
     let chordNames = Object.keys(chords);
     for (let i = 0; i < chordNames.length; i++) {
-        // 这里您可能需要调整方块的初始位置和大小
         blocks.push(new DraggableBlock(i * 100 + 10, i * 60 + 10, 50, 20, chordNames[i]));
     }
-}
+} // function that push all blocks into an array
 
 
   function mousePressed() {
@@ -342,6 +326,7 @@ function initializeBlocks() {
       block.released();
     }
   }
+
   function updateCursorForBlocks() {
     let cursorUpdated = false;
   
